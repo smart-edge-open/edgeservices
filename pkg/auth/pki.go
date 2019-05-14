c4331eb5ad0b67d6e5763535acc0eab379a0d90c
PatrykDiak <47568946+PatrykDiak@users.noreply.github.com>
2019-05-14 08:59:45 +0200

    PKI file helper (#74)
    * Initial PKI file helper
    
    * SaveKey, LoadKey tests
    
    * Refactor
    
    * Cert management tests

---
+++ pkg/auth/pki.go
@@ -0,0 +1,166 @@
+// Copyright 2019 Intel Corporation and Smart-Edge.com, Inc. All rights reserved
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package auth
+
+import (
+	"crypto"
+	"crypto/x509"
+	"encoding/pem"
+	"io/ioutil"
+	"os"
+
+	"github.com/pkg/errors"
+)
+
+const filePerm = os.FileMode(0600)
+
+// readFileWithPerm reads a file after veryfying permissions
+func readFileWithPerm(path string, perm os.FileMode) ([]byte, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, errors.Wrapf(err, "Failed to open %s", path)
+	}
+	defer f.Close()
+
+	stat, err := f.Stat()
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to get file info")
+	}
+	fPerm := stat.Mode().Perm()
+	if fPerm != perm {
+		return nil, errors.Errorf(
+			"Invalid file permissions. Got: %o Expected: %o", fPerm, perm)
+	}
+
+	return ioutil.ReadAll(f)
+}
+
+// LoadKey verifies file permissions(0600) and loads a PEM encoded PKCS#8 key
+func LoadKey(path string) (crypto.PrivateKey, error) {
+	data, err := readFileWithPerm(path, filePerm)
+	if err != nil {
+		return nil, err
+	}
+
+	block, _ := pem.Decode(data)
+	if block == nil {
+		return nil, errors.New("Failed to decode key")
+	}
+	if block.Type != "PRIVATE KEY" {
+		return nil, errors.Errorf("%s is not a key", path)
+	}
+
+	return x509.ParsePKCS8PrivateKey(block.Bytes)
+}
+
+// SaveKey saves PEM encoded PKCS#8 key to a file with permissions set to 0600
+func SaveKey(key crypto.PrivateKey, path string) error {
+	der, err := x509.MarshalPKCS8PrivateKey(key)
+	if err != nil {
+		return errors.Wrap(err, "Failed to marshal key")
+	}
+	data := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "PRIVATE KEY",
+			Bytes: der,
+		},
+	)
+	if data == nil {
+		return errors.Wrap(err, "Failed to encode key")
+	}
+
+	f, err := os.Create(path)
+	if err != nil {
+		return errors.Wrapf(err, "Failed to open %s", path)
+	}
+	if err = f.Chmod(filePerm); err != nil {
+		f.Close()
+		return errors.Wrap(err, "Failed to set file permissions")
+	}
+	if _, err = f.Write(data); err != nil {
+		f.Close()
+		return errors.Wrap(err, "Failed to write key to file")
+	}
+	return f.Close()
+}
+
+// LoadCert verifies file permissions(0600) and loads a certificate
+func LoadCert(path string) (*x509.Certificate, error) {
+	certs, err := LoadCerts(path)
+	if err != nil {
+		return nil, err
+	}
+	return certs[0], nil
+}
+
+// LoadCerts verifies file permissions(0600) and loads all certificates
+// If no certificates are found returns an error
+func LoadCerts(path string) ([]*x509.Certificate, error) {
+	var certs []*x509.Certificate
+
+	data, err := readFileWithPerm(path, filePerm)
+	if err != nil {
+		return nil, err
+	}
+
+	for len(data) > 0 {
+		var block *pem.Block
+		block, data = pem.Decode(data)
+		if block == nil {
+			return nil, errors.New("Failed to decode PEM block")
+		}
+		if block.Type != "CERTIFICATE" {
+			continue
+		}
+
+		cert, err := x509.ParseCertificate(block.Bytes)
+		if err != nil {
+			return nil, errors.Wrap(err, "Failed to parse certificate")
+		}
+		certs = append(certs, cert)
+	}
+
+	if len(certs) == 0 {
+		return nil, errors.New("Failed to load any certificates")
+	}
+
+	return certs, nil
+}
+
+// SaveCert saves PEM encoded certificate to a file with permissions set to 0600
+func SaveCert(path string, certs ...*x509.Certificate) error {
+	f, err := os.Create(path)
+	if err != nil {
+		return errors.Wrapf(err, "Failed to open %s", path)
+	}
+	if err = f.Chmod(filePerm); err != nil {
+		f.Close()
+		return errors.Wrap(err, "Failed to set file permissions")
+	}
+
+	for _, cert := range certs {
+		if err = pem.Encode(
+			f,
+			&pem.Block{
+				Type:  "CERTIFICATE",
+				Bytes: cert.Raw,
+			},
+		); err != nil {
+			f.Close()
+			return errors.Wrapf(err, "Failed to encode certificate to file")
+		}
+	}
+	return f.Close()
+}
---
+++ pkg/auth/pki_test.go
@@ -0,0 +1,225 @@
+// Copyright 2019 Intel Corporation and Smart-Edge.com, Inc. All rights reserved
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package auth_test
+
+import (
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"fmt"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"io/ioutil"
+	"math/big"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/smartedgemec/appliance-ce/pkg/auth"
+)
+
+func TestPKI(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "PKI Suite")
+}
+
+func genCert() (*x509.Certificate, error) {
+	key, err := ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
+	if err != nil {
+		return nil, err
+	}
+	template := x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		Subject: pkix.Name{
+			Organization: []string{"Test"},
+		},
+		NotBefore: time.Now(),
+		NotAfter:  time.Now().Add(time.Hour),
+	}
+
+	der, err := x509.CreateCertificate(rand.Reader, &template, &template,
+		&key.PublicKey, key)
+	if err != nil {
+		return nil, err
+	}
+	return x509.ParseCertificate(der)
+}
+
+var _ = Describe("Key management", func() {
+	var (
+		key        crypto.PrivateKey
+		encodedKey []byte
+		keyPath    = filepath.Join(os.TempDir(), "key.pem")
+	)
+
+	BeforeEach(func() {
+		var err error
+		key, err = ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+		Expect(err).ToNot(HaveOccurred())
+		der, err := x509.MarshalPKCS8PrivateKey(key)
+		Expect(err).ToNot(HaveOccurred())
+		encodedKey = pem.EncodeToMemory(
+			&pem.Block{
+				Type:  "PRIVATE KEY",
+				Bytes: der,
+			},
+		)
+		Expect(encodedKey).ToNot(BeNil())
+
+	})
+	AfterEach(func() {
+		os.Remove(keyPath)
+	})
+	Describe("LoadKey", func() {
+		It("Should load a key from file", func() {
+			err := ioutil.WriteFile(keyPath,
+				encodedKey, os.FileMode(0644))
+			Expect(err).ToNot(HaveOccurred())
+
+			By("Verifying file permissions")
+			_, err = auth.LoadKey(keyPath)
+			Expect(err).To(HaveOccurred())
+
+			err = os.Chmod(keyPath, os.FileMode(0600))
+			Expect(err).ToNot(HaveOccurred())
+
+			By("Loading key")
+			loadedKey, err := auth.LoadKey(keyPath)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(loadedKey).To(Equal(key))
+
+		})
+	})
+
+	Describe("SaveKey", func() {
+		It("Should save a key to file", func() {
+			By("Saving key")
+			err := auth.SaveKey(key, keyPath)
+			Expect(err).ToNot(HaveOccurred())
+
+			By("Setting file permissions")
+			stat, err := os.Stat(keyPath)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(stat.Mode().Perm()).To(Equal(os.FileMode(0600)))
+
+			data, err := ioutil.ReadFile(keyPath)
+			Expect(err).ToNot(HaveOccurred())
+
+			Expect(data).To(Equal(encodedKey))
+		})
+	})
+})
+
+var _ = Describe("Cert management", func() {
+	var (
+		cert        *x509.Certificate
+		encodedCert []byte
+		certPath    = filepath.Join(os.TempDir(), "cert.pem")
+	)
+
+	BeforeEach(func() {
+		var err error
+		cert, err = genCert()
+		Expect(err).ToNot(HaveOccurred())
+		encodedCert = pem.EncodeToMemory(
+			&pem.Block{
+				Type:  "CERTIFICATE",
+				Bytes: cert.Raw,
+			},
+		)
+		Expect(encodedCert).ToNot(BeNil())
+	})
+	AfterEach(func() {
+		os.Remove(certPath)
+	})
+
+	Describe("LoadCert", func() {
+		It("Should load a certificate from file", func() {
+			err := ioutil.WriteFile(certPath,
+				encodedCert, os.FileMode(0644))
+			Expect(err).ToNot(HaveOccurred())
+
+			By("Verifying file permissions")
+			_, err = auth.LoadCert(certPath)
+			Expect(err).To(HaveOccurred())
+
+			err = os.Chmod(certPath, os.FileMode(0600))
+			Expect(err).ToNot(HaveOccurred())
+
+			By("Loading cert")
+			loadedCert, err := auth.LoadCert(certPath)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(loadedCert).To(Equal(cert))
+		})
+	})
+
+	Describe("LoadCerts", func() {
+		It("Should load certificates from file", func() {
+			err := ioutil.WriteFile(certPath,
+				[]byte{}, os.FileMode(0600))
+			Expect(err).ToNot(HaveOccurred())
+
+			By("Verifying a file with no certificates")
+			_, err = auth.LoadCerts(certPath)
+			Expect(err).To(HaveOccurred())
+
+			os.Remove(certPath)
+			data := append(encodedCert, encodedCert...)
+			err = ioutil.WriteFile(certPath,
+				data, os.FileMode(0644))
+			Expect(err).ToNot(HaveOccurred())
+
+			By("Verifying file permissions")
+			fmt.Println(certPath)
+			_, err = auth.LoadCerts(certPath)
+			Expect(err).To(HaveOccurred())
+
+			err = os.Chmod(certPath, os.FileMode(0600))
+			Expect(err).ToNot(HaveOccurred())
+
+			By("Loading certs")
+			_, err = auth.LoadCerts(certPath)
+			Expect(err).ToNot(HaveOccurred())
+			loadedCerts, err := auth.LoadCerts(certPath)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(len(loadedCerts)).To(Equal(2))
+			Expect(loadedCerts[0]).To(Equal(cert))
+			Expect(loadedCerts[1]).To(Equal(cert))
+		})
+	})
+
+	Describe("SaveCert", func() {
+		It("Should save a cerificate to file", func() {
+			By("Saving certificate")
+			err := auth.SaveCert(certPath, cert)
+			Expect(err).ToNot(HaveOccurred())
+
+			By("Setting file permissions")
+			stat, err := os.Stat(certPath)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(stat.Mode().Perm()).To(Equal(os.FileMode(0600)))
+
+			data, err := ioutil.ReadFile(certPath)
+			Expect(err).ToNot(HaveOccurred())
+
+			Expect(data).To(Equal(encodedCert))
+		})
+	})
+})
