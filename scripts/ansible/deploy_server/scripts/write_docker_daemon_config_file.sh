0337a76cb48a2f4a40f9c3130f5322ecc20e0a48
PatrykDiak <47568946+PatrykDiak@users.noreply.github.com>
2019-06-14 07:07:04 +0200

    Disable docker userland proxy to allow source IP detection (#152)

--- scripts/ansible/deploy_server/scripts/write_docker_daemon_config_file.sh
+++ scripts/ansible/deploy_server/scripts/write_docker_daemon_config_file.sh
@@ -3,6 +3,7 @@ cat >/etc/docker/daemon.json <<EOL
 {
   "bridge": "virbr0",
   "fixed-cidr": "${1}",
-  "dns": ["192.168.122.1", "8.8.8.8"]
+  "dns": ["192.168.122.1", "8.8.8.8"],
+  "userland-proxy": false
 }
 EOL
