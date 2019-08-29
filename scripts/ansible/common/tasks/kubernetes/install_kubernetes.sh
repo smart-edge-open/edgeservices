5994773d23dc7713a9418dae727c79f8912cc4d0
ipatrykx <53896266+ipatrykx@users.noreply.github.com>
2019-08-29 16:13:52 +0200

    OP-1019: Develop an Ansible script for setting up K8s Worker (#19)
    * Ansible scripts for Kubernetes installation on EdgeNode
    
    * Moved Kubernetes task to common folder. Added scripts for deploy server and build server.
    
    * Reverted defaults.yml to previous revision
    
    * Moved disable swap action to separate files.
    Removed redundant 'disable SELinux' task.
    Removed reduntant comments.
    Moved Kubernetes enable action to separate file and used Ansible syntax.
    Moved iptables setup to separate file and used Ansible syntax.
    Added missing variables.
    Added action that adds KUBELET_EXTRA_ARGS to 10-kubeadm.conf.
    
    * Added missing exclude
    
    * Changed uninstall name tag
    
    * Moved Kubernetes installation script to kubernetes folder
    
    * Added log for Kubernetes installation
    
    * Added script to open firewall's port.
    Changed libvirt to Kubernetes in iptables file.

--- scripts/ansible/common/tasks/kubernetes/install_kubernetes.sh
+++ scripts/ansible/common/tasks/kubernetes/install_kubernetes.sh
@@ -1,10 +1,12 @@
+#!/usr/bin/env bash
+#
 # Copyright 2019 Intel Corporation and Smart-Edge.com, Inc. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,20 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Enable logging of screen output (Ansible scripts) to files in given folder.
-# Logs will be saved to folder: <appliance-ce_repository/scripts/ansible/logs/
+source ../../scripts/ansible-precheck.sh
 
-currdir=${PWD##*/} 
-
-if [ -z "$1" ]
-then
-    BASE_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
-else
-    BASE_PATH="$1"
-fi
-
-[ -d ${BASE_PATH}/../logs ] || mkdir  ${BASE_PATH}/../logs
-FILENAME="`date +%Y-%m-%d_%H-%M-%S_ansible.log`"
-
-# Comment out below export to disable console logs saving to files.
-export ANSIBLE_LOG_PATH="${BASE_PATH}/../logs/${FILENAME}"
+source ../../vars/task_log_file.sh ../..
+ansible-playbook setup_kubernetes.yml -i ../../vars/hosts --connection=local
