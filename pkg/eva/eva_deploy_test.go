4fd87aeeb1e9f1fa0e27ff7bd0999db8d17eff99
groclawski <53475993+groclawski@users.noreply.github.com>
2019-10-25 14:47:47 +0200

    Eva deploy libvirt tests (#92)
    * adding libvirt tests
    
    * refactoring docker tests
    
    * changing eva.json for current test cases

--- pkg/eva/eva_deploy_test.go
+++ pkg/eva/eva_deploy_test.go
@@ -32,9 +32,10 @@ import (
 	evapb "github.com/otcshare/edgenode/pkg/eva/pb"
 	"github.com/pkg/errors"
 	"google.golang.org/grpc"
+	"github.com/otcshare/edgenode/pkg/eva"
 )
 
-var _ = Describe("EVA: DeployContainer", func() {
+var _ = Describe("EVA docker tests", func() {
 	stopInd := make(chan bool)
 
 	BeforeEach(func() {
@@ -253,25 +254,6 @@ var _ = Describe("EVA: DeployContainer", func() {
 				})
 			})
 	})
-})
-
-var _ = Describe("EVA: Redeploy", func() {
-	stopInd := make(chan bool)
-
-	BeforeEach(func() {
-		err := runEVA("testdata/eva.json", stopInd)
-		Expect(err).ToNot(HaveOccurred())
-		// Clean directories
-		os.RemoveAll(cfgFile.CertsDir)
-		os.RemoveAll(cfgFile.AppImageDir)
-		metadatahelpers.CreateDir(cfgFile.AppImageDir)
-		stubs.HTTPCliStub = stubs.HTTPClientStub{}
-		stubs.DockerCliStub = stubs.DockerClientStub{}
-	})
-
-	AfterEach(func() {
-		stopEVA(stopInd)
-	})
 
 	When("Redeploy is called", func() {
 		Context("for container app with correct arguments", func() {
@@ -341,25 +323,6 @@ var _ = Describe("EVA: Redeploy", func() {
 			})
 		})
 	})
-})
-
-var _ = Describe("EVA: Undeploy", func() {
-	stopInd := make(chan bool)
-
-	BeforeEach(func() {
-		err := runEVA("testdata/eva.json", stopInd)
-		Expect(err).ToNot(HaveOccurred())
-		// Clean directories
-		os.RemoveAll(cfgFile.CertsDir)
-		os.RemoveAll(cfgFile.AppImageDir)
-		metadatahelpers.CreateDir(cfgFile.AppImageDir)
-		stubs.HTTPCliStub = stubs.HTTPClientStub{}
-		stubs.DockerCliStub = stubs.DockerClientStub{}
-	})
-
-	AfterEach(func() {
-		stopEVA(stopInd)
-	})
 
 	When("Undeploy is called", func() {
 		Context("for container app with correct arguments", func() {
@@ -411,3 +374,192 @@ var _ = Describe("EVA: Undeploy", func() {
 		})
 	})
 })
+
+var _ = Describe("EVA Libvirt tests", func() {
+
+	stopInd := make(chan bool)
+
+	BeforeEach(func() {
+		err := runEVA("testdata/eva.json", stopInd)
+		Expect(err).ToNot(HaveOccurred())
+		// Clean directories
+		os.RemoveAll(cfgFile.CertsDir)
+		os.RemoveAll(cfgFile.AppImageDir)
+		metadatahelpers.CreateDir(cfgFile.AppImageDir)
+		stubs.HTTPCliStub = stubs.HTTPClientStub{}
+		stubs.ConnStub = stubs.LibvirtConnectStub{}
+		stubs.DomStub = stubs.LibvirtDomainStub{}
+	})
+
+	AfterEach(func() {
+		stopEVA(stopInd)
+	})
+
+	When("Deploy is called", func() {
+		Context("for vm with correct arguments", func() {
+			It("responds with no error", func() {
+
+				wrappers.CreateHTTPClient = stubs.CreateHTTPClientStub
+				eva.CreateLibvirtConnection = stubs.CreateLibvirtConnectionStub
+				stubs.ConnStub.DomByName = stubs.DomStub
+
+				body := ioutil.NopCloser(strings.NewReader("TEST IMAGE"))
+				stubs.HTTPCliStub.HTTPResp = http.Response{Status: "200 OK",
+					StatusCode: 200, Proto: "HTTP/1.1", ProtoMajor: 1,
+					ProtoMinor: 1, Body: body, ContentLength: 11}
+
+				// Create connection
+				conn, cancelTimeout, prefaceLis := createConnection()
+				defer cancelTimeout()
+				defer prefaceLis.Close()
+				defer conn.Close()
+
+				client := evapb.NewApplicationDeploymentServiceClient(conn)
+
+				ctx, cancel := context.WithTimeout(context.Background(),
+					10*time.Second)
+				defer cancel()
+
+				uri := evapb.Application_HttpUri{
+					HttpUri: &evapb.Application_HTTPSource{
+						HttpUri: "https://localhost/test_img.tar.gz"},
+				}
+				app := evapb.Application{Id: "test-app-deploy",
+					Cores: 2, Memory: 40, Source: &uri}
+
+				_, err := client.DeployVM(ctx, &app,
+					grpc.WaitForReady(true))
+				Expect(err).ToNot(HaveOccurred())
+
+				time.Sleep(100 * time.Millisecond)
+
+				// Verify status after deployment
+				appid := evapb.ApplicationID{Id: "test-app-deploy"}
+				alsClient := evapb.NewApplicationLifecycleServiceClient(conn)
+				status, err := alsClient.GetStatus(ctx, &appid,
+					grpc.WaitForReady(true))
+				Expect(err).ToNot(HaveOccurred())
+				Expect(status.Status).To(Equal(evapb.LifecycleStatus_READY))
+
+			})
+		})
+	})
+
+	When("Undeploy is called", func() {
+		Context("for vm with correct arguments", func() {
+			It("responds with no error", func() {
+
+				wrappers.CreateHTTPClient = stubs.CreateHTTPClientStub
+				eva.CreateLibvirtConnection = stubs.CreateLibvirtConnectionStub
+				stubs.ConnStub.DomByName = stubs.DomStub
+
+				appid := evapb.ApplicationID{Id: "test-app-undeploy"}
+
+				expectedAppPath := filepath.Join(cfgFile.AppImageDir,
+					"test-app-undeploy")
+				metadatahelpers.CreateDir(expectedAppPath)
+				metadatahelpers.CreateFile(filepath.Join(expectedAppPath,
+					"metadata.json"),
+					`{"Type":"LibvirtDomain","URL":"https://localhost/test_`+
+						`img.tar.gz","App":{"id":"test-app-undeploy","cores":`+
+						`2,"memory":40,"status":2,"Source":null}}`)
+				metadatahelpers.CreateFile(
+					filepath.Join(expectedAppPath, "deployed"),
+					"test-app-undeploy\n")
+				metadatahelpers.CreateFile(
+					filepath.Join(expectedAppPath, "test_img.tar.gz"),
+					"TEST IMAGE")
+
+				// Create connection
+				conn, cancelTimeout, prefaceLis := createConnection()
+				defer cancelTimeout()
+				defer prefaceLis.Close()
+				defer conn.Close()
+
+				client := evapb.NewApplicationDeploymentServiceClient(conn)
+				ctx, cancel := context.WithTimeout(context.Background(),
+					10*time.Second)
+				defer cancel()
+				_, err := client.Undeploy(ctx, &appid, grpc.WaitForReady(true))
+				Expect(err).ToNot(HaveOccurred())
+
+				time.Sleep(100 * time.Millisecond)
+
+				// Verify status after undeployment
+				alsClient := evapb.NewApplicationLifecycleServiceClient(conn)
+				status, err := alsClient.GetStatus(ctx, &appid,
+					grpc.WaitForReady(true))
+				Expect(err)
+				Expect(status).To(BeNil())
+
+			})
+		})
+	})
+
+	When("Redeploy is called", func() {
+		Context("for vm with correct arguments", func() {
+			It("responds with no error", func() {
+				
+				body := ioutil.NopCloser(strings.NewReader("TEST IMAGE"))
+				stubs.HTTPCliStub.HTTPResp = http.Response{Status: "200 OK",
+					StatusCode: 200, Proto: "HTTP/1.1", ProtoMajor: 1,
+					ProtoMinor: 1, Body: body, ContentLength: 11}
+
+				wrappers.CreateHTTPClient = stubs.CreateHTTPClientStub
+				eva.CreateLibvirtConnection = stubs.CreateLibvirtConnectionStub
+				stubs.ConnStub.DomByName = stubs.DomStub
+
+				expectedAppPath := filepath.Join(cfgFile.AppImageDir,
+					"test-app-redeploy")
+				metadatahelpers.CreateDir(expectedAppPath)
+				metadatahelpers.CreateFile(filepath.Join(expectedAppPath,
+					"metadata.json"),
+					`{"Type":"LibvirtDomain","URL":"https://localhost/test_`+
+						`img.tar.gz","App":{"id":"test-app-redeploy","cores":`+
+						`2,"memory":40,"status":2,"Source":null}}`)
+				metadatahelpers.CreateFile(
+					filepath.Join(expectedAppPath, "deployed"),
+					"test-app-redeploy\n")
+				metadatahelpers.CreateFile(
+					filepath.Join(expectedAppPath, "test_img.tar.gz"),
+					"TEST IMAGE")
+
+				// Create connection
+				conn, cancelTimeout, prefaceLis := createConnection()
+				defer cancelTimeout()
+				defer prefaceLis.Close()
+				defer conn.Close()
+
+				uri := evapb.Application_HttpUri{
+					HttpUri: &evapb.Application_HTTPSource{
+						HttpUri: "https://localhost/test_img.tar.gz"},
+				}
+
+				app := evapb.Application{Id: "test-app-redeploy",
+					Cores:  2,
+					Memory: 40,
+					Source: &uri}
+
+				client := evapb.NewApplicationDeploymentServiceClient(conn)
+				ctx, cancel := context.WithTimeout(context.Background(),
+					10*time.Second)
+				defer cancel()
+				_, err := client.Redeploy(ctx, &app, grpc.WaitForReady(true))
+				Expect(err).ToNot(HaveOccurred())
+
+				time.Sleep(100 * time.Millisecond)
+
+				// Verify status after deployment
+				appid := evapb.ApplicationID{Id: "test-app-redeploy"}
+				alsClient := evapb.NewApplicationLifecycleServiceClient(conn)
+				status, err := alsClient.GetStatus(ctx, &appid,
+					grpc.WaitForReady(true))
+				Expect(err).ToNot(HaveOccurred())
+				Expect(status.Status).To(Equal(evapb.LifecycleStatus_READY))
+
+			})
+		})
+	})
+
+})
+
