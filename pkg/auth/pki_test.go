f542e120965d510a985c3d5b08e66646caa05d6a
Jiang Zhenggang <zhenggang1x.jiang@intel.com>
2020-08-25 13:37:24 +0800

    [4086] Auth Package UT add a error check

--- pkg/auth/pki_test.go
+++ pkg/auth/pki_test.go
@@ -154,6 +154,7 @@ var _ = Describe("Key management", func() {
 			Expect(err).ToNot(HaveOccurred())
 			err = ioutil.WriteFile(keyPath,
 				encodedKey1, os.FileMode(0600))
+			Expect(err).ToNot(HaveOccurred())
 			loadedKey, err = auth.LoadKey(keyPath)
 			Expect(err).To(HaveOccurred())
 		})
